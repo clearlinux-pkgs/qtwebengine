df10ae549df1c22e9ea6b11448ddf56429750a6e
Thiago Macieira <thiago.macieira@intel.com>
2019-12-11 20:47:32 -0800

    Update update_changelog.sh

--- update_changelog.sh
+++ update_changelog.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-# Get version from Makefile
-eval `sed -En '/URL\s*:?=\s*/{s///;s,.*-([0-9.]+)-clean\.tar.xz,VERSION=\1,p;}' Makefile`
+# Get name and version from Makefile
+eval `sed -En '/^PKG_NAME\s*:?=\s*/s//PKG_NAME=/p
+/URL\s*:?=\s*/{s///;s,.*-([0-9.]+)\.tar.xz,VERSION=\1,p;}' Makefile`
 
-PKG_NAME=qtwebengine
 CHANGES_FILE=https://code.qt.io/cgit/qt/$PKG_NAME.git/plain/dist/changes-$VERSION?h=v$VERSION
 
-rm -f NEWS ChangeLog
-wget -O ChangeLog "$CHANGES_FILE" || \
-    echo > ChangeLog "No changelog file found for $PKG_NAME $VERSION"
+git rm -f NEWS ChangeLog
+if wget -O ChangeLog "$CHANGES_FILE"; then
+    git add ChangeLog
+fi
+exit 0
